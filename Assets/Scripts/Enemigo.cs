7b59e5d060a6e2502051afa47e27e2829ad52124
alejandrittaa <alejandracortesreyero@gmail.com>
2024-12-25 18:45:51 +0100

    guardado

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -8,15 +8,22 @@ public class Enemigo : MonoBehaviour
 
     private Animator anim;
     [SerializeField] private NavMeshAgent agent;
+    private Player player;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+
+        //para que identifique donde está el player
+        player = GameObject.FindObjectOfType<Player>();
     }
 
     void Update()
     {
         //dividimos la velocidad actual entre la velocidad máxima
         anim.SetFloat("velocity", agent.velocity.magnitude / agent.speed);
+
+        //para que vaya a por el player
+        agent.SetDestination(player.gameObject.transform.position);
     }
 }
